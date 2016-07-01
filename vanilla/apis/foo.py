b0b29695cab229b1abeac932353ca0f145a817b2
pdonorio <tempo.pause@gmail.com>
2016-07-01 16:16:47 +0200

    Testing elastic search service connection

--- vanilla/apis/foo.py
+++ vanilla/apis/foo.py
@@ -21,6 +21,19 @@ class SomeRestEndpoint(ExtendedApiResource):
 
     @decorate.apimethod
     def get(self):
+
         hello = "Hello world"
         logger.info(hello)
+
+        # ####################
+        # # Test graph
+        # graph = self.global_get_service('neo4j')
+        # queryout = graph.cypher("MATCH (n) RETURN (n)")
+        # print(queryout)
+
+        ####################
+        # Test elastic
+        elastic = self.global_get_service('elasticsearch')
+        print(elastic)
+
         return self.response(hello)
