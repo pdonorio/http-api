42dbfc0d02101f3bcc326d80c96d0cf6a5dcd385
pdonorio <tempo.pause@gmail.com>
2016-07-19 11:54:41 +0200

    Adding a test case for listing services with docker compose

--- notebooks/test_compose.py
+++ notebooks/test_compose.py
@@ -6,6 +6,9 @@ Testing compose commands from python
 
 """
 
+class mem:
+    pass
+
 #############################
 # from __future__ import absolute_import
 
@@ -29,6 +32,8 @@ def compose_com(cli_options):
     # exit(1)
 
     project = project_from_options('.', options)
+    mem.current_project = project
+
     command = TopLevelCommand(project)
 
     with errors.handle_connection_errors(project.client):
@@ -37,8 +42,35 @@ def compose_com(cli_options):
     return out
 
 
+import contextlib
+import sys
+
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+
+@contextlib.contextmanager
+def nostdout():
+    save_stdout = sys.stdout
+    sys.stdout = StringIO()
+    sys.stderr = StringIO()
+    yield
+    sys.stdout = save_stdout
+
 #############################
 
-# Detached command doesn't return any output
+# Launch a ps empty command just to save the project
+with nostdout():
+    compose_com(['ps'])
+
+# use the current project to list services
+for service in mem.current_project.services:
+    print("SERVICE", service)
+
+## TO BE REMOVED
+exit(0)
+
 compose_com(['up', '-d', 'graphdb'])
 compose_com(['ps'])
